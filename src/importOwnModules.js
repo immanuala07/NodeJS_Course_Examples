05b0c9a3f337dce132f55b4c8f449c65b5d849b9
Immanual <immanualatrinity@gmail.com>
2021-01-24 13:33:42 +0530

    Importing our own files

---
+++ src/importOwnModules.js
@@ -0,0 +1,11 @@
+// const name = require('./utils.js')
+// console.log(name)
+
+
+// const add = require('./utils.js')
+// const sum = add(6,-2)
+// console.log(sum)
+
+const getNotes = require('./utils.js')
+const msg = getNotes()
+console.log(msg)
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+console.log('utils.js')
+
+const name = 'Immanual'
+
+const add = function(a, b) {
+    return a+b
+} 
+
+const getNotes = function() {
+    return 'your notes...'
+}
+
+module.exports = getNotes
