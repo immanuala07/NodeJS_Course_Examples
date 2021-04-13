db248f5ef8c1e73b79610dfd3d48c21cf68ac529
Immanual <immanualatrinity@gmail.com>
2021-04-13 20:05:16 +0530

    code modified

--- src/unit_15-Weather-app/app.js
+++ src/unit_15-Weather-app/app.js
@@ -5,8 +5,9 @@
  * Event-driven
  */
 
-console.log("Starting");
-setTimeout(() => {
-	console.log('2 second timer');
-}, 2000);
-console.log('Stopping');
+// Example of Asynchronous code
+// console.log("Starting");
+// setTimeout(() => {
+// 	console.log('2 second timer');
+// }, 2000);
+// console.log('Stopping');
